25ab03bb0d134b89507ca5ba3a3b67bc999eaa7b
Denys Mitlenko <softonka@gmail.com>
2023-02-12 16:17:25 +0200

    Added tests

---
+++ CipherLabs - Tables Test/DoublePermutationTablesTest.cs
@@ -0,0 +1,120 @@
+﻿using CipherLabs.Core;
+using CipherLabs.Core.Algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CipherLabs___Tables_Test
+{
+    [TestClass]
+    public class DoublePermutationTablesTest
+    {
+
+        [TestMethod]
+        [TestCategory("DoublePermutationTable - Exceptions")]
+        [ExpectedException(typeof(CipherException), "One of the keywords is empty")]
+        public void EmptyKeywordTest1()
+        {
+            new DoublePermutationTable().Encode("Test", "12", "");
+        }
+
+        [TestMethod]
+        [TestCategory("DoublePermutationTable - Exceptions")]
+        [ExpectedException(typeof(CipherException), "One of the keywords is empty")]
+        public void EmptyKeywordTest2()
+        {
+            new DoublePermutationTable().Encode("Test", "", "12");
+        }
+
+        [TestMethod]
+        [TestCategory("DoublePermutationTable - Exceptions")]
+        [ExpectedException(typeof(CipherException), "One of the keywords contain not allowed symbols")]
+        public void UnallowedCharactersTest1()
+        {
+            new DoublePermutationTable().Encode("Test", "12q", "12");
+        }
+
+        [TestMethod]
+        [TestCategory("DoublePermutationTable - Exceptions")]
+        [ExpectedException(typeof(CipherException), "One of the keywords contain not allowed symbols")]
+        public void UnallowedCharactersTest2()
+        {
+            new DoublePermutationTable().Encode("Test", "12", "12q");
+        }
+
+        [TestMethod]
+        [TestCategory("DoublePermutationTable - Exceptions")]
+        [ExpectedException(typeof(CipherException), "Vertical keyword contains non-continous order")]
+        public void NonContinousVerticalTest1()
+        {
+            new DoublePermutationTable().Encode("Test", "124", "12");
+        }
+
+        [TestMethod]
+        [TestCategory("DoublePermutationTable - Exceptions")]
+        [ExpectedException(typeof(CipherException), "Vertical keyword contains non-continous order")]
+        public void NonContinousVerticalTest2()
+        {
+            new DoublePermutationTable().Encode("Test", "23", "12");
+        }
+
+        [TestMethod]
+        [TestCategory("DoublePermutationTable - Exceptions")]
+        [ExpectedException(typeof(CipherException), "Vertical keyword contains non-continous order")]
+        public void NonContinousVerticalTest3()
+        {
+            new DoublePermutationTable().Encode("Test", "122", "12");
+        }
+
+        [TestMethod]
+        [TestCategory("DoublePermutationTable - Exceptions")]
+        [ExpectedException(typeof(CipherException), "Horizontal keyword contains non-continous order")]
+        public void NonContinousHorizontalTest1()
+        {
+            new DoublePermutationTable().Encode("Test", "12", "124");
+        }
+
+        [TestMethod]
+        [TestCategory("DoublePermutationTable - Exceptions")]
+        [ExpectedException(typeof(CipherException), "Horizontal keyword contains non-continous order")]
+        public void NonContinousHorizontalTest2()
+        {
+            new DoublePermutationTable().Encode("Test", "12", "23");
+        }
+
+        [TestMethod]
+        [TestCategory("DoublePermutationTable - Exceptions")]
+        [ExpectedException(typeof(CipherException), "Horizontal keyword contains non-continous order")]
+        public void NonContinousHorizontalTest3()
+        {
+            new DoublePermutationTable().Encode("Test", "12", "122");
+        }
+
+        [TestMethod]
+        [TestCategory("DoublePermutationTable - Exceptions")]
+        [ExpectedException(typeof(CipherException), "Keywords is too short, may be a data loss")]
+        public void ShortKeywordsTest()
+        {
+            new DoublePermutationTable().Encode("This is to much text for you :)", "12", "12");
+        }
+
+        [TestMethod]
+        [TestCategory("DoublePermutationTable - Encode")]
+        [DataRow("i want some cheese", "4132", "23415", "o_hateei_s__secwm_en")]
+        [DataRow("i'd like to drink some sweet cola drink", "1437256", "415236", "'erelno_tnt__el_kod__iwdkoisdei_akmic__r_s")]
+        [DataRow("КОЛА МОЛА ЛА-ЛА-ЛА", "54321", "12345", "_-ЛМК_ЛАОО_А-ЛЛ__ЛАА__А__")]
+        public void EncodeTest(string phrase, string key1, string key2, string expected)
+        {
+            Assert.AreEqual(new DoublePermutationTable().Encode(phrase, key1, key2), expected);
+        }
+
+        [TestMethod]
+        [TestCategory("DoublePermutationTable - Decode")]
+        [DataRow("o_hateei_s__secwm_en", "4132", "23415", "i want some cheese")]
+        [DataRow("'erelno_tnt__el_kod__iwdkoisdei_akmic__r_s", "1437256", "415236", "i'd like to drink some sweet cola drink")]
+        [DataRow("_-ЛМК_ЛАОО_А-ЛЛ__ЛАА__А__", "54321", "12345", "КОЛА МОЛА ЛА-ЛА-ЛА")]
+        public void DecodeTest(string phrase, string key1, string key2, string expected)
+        {
+            Assert.AreEqual(new DoublePermutationTable().Decode(phrase, key1, key2), expected);
+        }
+    }
+}
---
+++ CipherLabs - Tables Test/EncyptionTablesTest.cs
@@ -0,0 +1,63 @@
+﻿using CipherLabs.Core;
+using CipherLabs.Core.Algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CipherLabs___Tables_Test
+{
+    [TestClass]
+    public class EncyptionTablesTest
+    {
+        [TestMethod]
+        [TestCategory("EncyptionTable - Exceptions")]
+        [ExpectedException(typeof(CipherException), "Keyword is empty or null")]
+        public void EmptyKeywordTest()
+        {
+            new EncryptionTable().Encode("", "", 2);
+        }
+
+        [TestMethod]
+        [TestCategory("EncyptionTable - Exceptions")]
+        [ExpectedException(typeof(CipherException), "Keyword has duplicate characters")]
+        public void DuplicateCharactersTest()
+        {
+            new EncryptionTable().Encode("", "kaka", 2);
+        }
+
+        [TestMethod]
+        [TestCategory("EncyptionTable - Exceptions")]
+        [ExpectedException(typeof(CipherException), "Rows count must be greater than 2")]
+        public void RowsCountTest()
+        {
+            new EncryptionTable().Encode("", "ka", 1);
+        }
+
+        [TestMethod]
+        [TestCategory("EncyptionTable - Exceptions")]
+        [ExpectedException(typeof(CipherException), "Rows count is short, may be a data loss")]
+        public void DataLossTest()
+        {
+            new EncryptionTable().Encode("tomuchtext", "ka", 2);
+        }
+
+        [TestMethod]
+        [TestCategory("EncyptionTable - Encode")]
+        [DataRow("ИСХОДНИЙ ТЕКСТ", "КЛЮЧ", 4, "ИДС_СНТТХИ_ЕОЙ_К")]
+        [DataRow("СИРНИЙ соус", "СИР", 4, "ИоСЙуИ_сРс_Н")]
+        [DataRow("hello world", "dumbface", 2, "dw_h_rol_o_e_l_l")]
+        public void EncodeTest(string phrase, string keyword, int rows, string expected)
+        {
+            Assert.AreEqual(new EncryptionTable().Encode(phrase, keyword, rows), expected);
+        }
+
+        [TestMethod]
+        [TestCategory("EncyptionTable - Decode")]
+        [DataRow("ИДС_СНТТХИ_ЕОЙ_К", "КЛЮЧ", 4, "ИСХОДНИЙ ТЕКСТ")]
+        [DataRow("ИоСЙуИ_сРс_Н", "СИР", 4, "СИРНИЙ соус")]
+        [DataRow("dw_h_rol_o_e_l_l", "dumbface", 2, "hello world")]
+        public void DecodeTest(string phrase, string keyword, int rows, string expected)
+        {
+            Assert.AreEqual(new EncryptionTable().Decode(phrase, keyword, rows), expected);
+        }
+    }
+}
