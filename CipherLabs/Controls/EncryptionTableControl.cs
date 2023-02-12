672f07e8e6462bd340bfe28427ec8a4e6f3c15ed
Denys Mitlenko <softonka@gmail.com>
2023-02-12 21:47:46 +0200

    Update EncryptionTableControl.cs

--- CipherLabs/Controls/EncryptionTableControl.cs
+++ CipherLabs/Controls/EncryptionTableControl.cs
@@ -1,13 +1,6 @@
 ﻿using CipherLabs.Core.Algorithms;
 using CipherLabs.Core;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CipherLabs.Controls
