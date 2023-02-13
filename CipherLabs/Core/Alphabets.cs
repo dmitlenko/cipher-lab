c9ff6cd6c92a23e12047a2d57f7e9a1db432a3d2
Denys Mitlenko <softonka@gmail.com>
2023-02-13 18:11:34 +0200

    Added new :abc: Alphabets with some special characters

--- CipherLabs/Core/Alphabets.cs
+++ CipherLabs/Core/Alphabets.cs
@@ -10,5 +10,8 @@ namespace CipherLabs.Core
     {
         public static string English { get => "abcdefghijklmnopqrstuvwxyz"; }
         public static string Ukrainian { get => "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя"; }
+        public static string Special { get => "#$%&()*,./:;?@[\\]^`{}~"; }
+        public static string EnglishSpecial { get => English + ' ' + Special; }
+        public static string UkrainianSpecial { get => Ukrainian + ' ' + Special; }
     }
 }
