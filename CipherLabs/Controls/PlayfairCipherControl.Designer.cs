177fa57bf88b2c85d9f847558c33ab9cc79afe9d
Denys Mitlenko <softonka@gmail.com>
2023-02-13 21:34:36 +0200

    Update PlayfairCipherControl.Designer.cs

--- CipherLabs/Controls/PlayfairCipherControl.Designer.cs
+++ CipherLabs/Controls/PlayfairCipherControl.Designer.cs
@@ -74,8 +74,8 @@
             this.alphabetCombo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.alphabetCombo.FormattingEnabled = true;
             this.alphabetCombo.Items.AddRange(new object[] {
-            "Aa-Zz",
-            "Аа-Яя"});
+            "Aa-Zz+",
+            "Аа-Яя+"});
             this.alphabetCombo.Location = new System.Drawing.Point(112, 0);
             this.alphabetCombo.Name = "alphabetCombo";
             this.alphabetCombo.Size = new System.Drawing.Size(136, 24);
